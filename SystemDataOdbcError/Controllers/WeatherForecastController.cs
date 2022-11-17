c5dd466c859d4571dfe1a71c89193c4b20a4d1fd
Micah Kimel <mkimel@terminix-triad.com>
2022-11-17 08:27:49 -0500

    test project

---
+++ SystemDataOdbcError/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystemDataOdbcError.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+            {
+                //THIS BREAKS HERE
+                using (OdbcConnection connection = new OdbcConnection("ENTER ODBC CONNECTION HERE"))
+                {
+                    OdbcDataAdapter adapter = new OdbcDataAdapter("SELECT * FROM EMPLOYEE", connection);
+
+                    try
+                    {
+                        connection.Open();
+                        //do stuff
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
